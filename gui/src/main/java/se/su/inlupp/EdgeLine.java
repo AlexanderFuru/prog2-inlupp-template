9ab9ae27dbdf2406c5eddc51e63d16693ac8e7ee
AlexanderFuru <doikarn94@gmail.com>
2026-05-21 22:24:54 +0200

    EdgeLine
    Skapade en ny klass, EdgeLine, som ska representera en tunnelbanelinje genom att rita en linje mellan två stationer.
    Linjerna kommer att vara bundna till stationerna och hänger med när man flyttar dem

---
+++ gui/src/main/java/se/su/inlupp/EdgeLine.java
@@ -0,0 +1,41 @@
+package se.su.inlupp;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+
+public class EdgeLine extends Line {
+    
+    private final GraphEdge<Station> edgeData;
+    private final StationView fromStationView;
+    private final StationView toStationView;
+
+    private final int lineWidth = 4;
+
+    public EdgeLine(StationView fromStationView, StationView toStationView, GraphEdge<Station> edgeData) {
+        this.fromStationView = fromStationView;
+        this.toStationView = toStationView;
+        this.edgeData = edgeData;
+
+        this.setStroke(Color.RED);
+        this.setStrokeWidth(lineWidth);
+
+        this.startXProperty().bind(fromStationView.centerXProperty());
+        this.startYProperty().bind(fromStationView.centerYProperty());
+        this.endXProperty().bind(fromStationView.centerXProperty());
+        this.endYProperty().bind(toStationView.centerYProperty());
+
+        this.toBack();
+    }
+
+    public GraphEdge<Station> getEdgeData() {
+        return edgeData;
+    }
+
+    public StationView getFromStationView() {
+        return fromStationView;
+    }
+
+    public StationView getTStationView() {
+        return toStationView;
+    }
+}
