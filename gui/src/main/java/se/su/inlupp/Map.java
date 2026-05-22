bbaf347025f0b398880f4e36c1a315279e5278b6
AlexanderFuru <doikarn94@gmail.com>
2026-05-22 20:15:17 +0200

    Dialog
    Skapade en massa logik för att hantera dialog mellan användare och systemet
    Använder nu dialog för att koppla ihop stationer med möjlighet att ange namn och vikt direkt istället

--- gui/src/main/java/se/su/inlupp/Map.java
+++ gui/src/main/java/se/su/inlupp/Map.java
@@ -3,10 +3,17 @@ package se.su.inlupp;
 import java.io.File;
 import java.util.Optional;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class Map extends Pane {
@@ -110,14 +117,18 @@ public class Map extends Pane {
         }
         else {
             if (firstSelectedStation == clickedStation) {
-                System.out.println("Connecting stations...Cannot select the same station, choose the second station");
+                showErrorAlert("Error", "Invalid connection", "A station cannot be connected to itself");
                 return;
             }
 
-            System.out.println("Second station has been selected: " + clickedStation.getStation().getName());
-            se.su.inlupp.GraphEdge<se.su.inlupp.Station> testEdge = new se.su.inlupp.GraphEdge<Station>(clickedStation.getStation(), "TestLine", 5);
+            Optional<LineInputData> input = showConnectDialog(firstSelectedStation.getStation().getName(), clickedStation.getStation().getName());
+            if (input.isPresent()) {
+                LineInputData inputData = input.get();
 
-            connectStations(firstSelectedStation, clickedStation, testEdge);
+                se.su.inlupp.GraphEdge<se.su.inlupp.Station> testEdge = new se.su.inlupp.GraphEdge<>(clickedStation.getStation(), inputData.name, inputData.weight);
+                System.out.println("Second station has been selected: " + clickedStation.getStation().getName());
+                connectStations(firstSelectedStation, clickedStation, testEdge);
+            }
             
             firstSelectedStation.setFill(javafx.scene.paint.Color.WHITE);
             System.out.println("Stations " + firstSelectedStation.getStation().getName() + " and " + clickedStation.getStation().getName() + " have been connected");
@@ -133,5 +144,76 @@ public class Map extends Pane {
         this.getChildren().add(edgeView);
 
         edgeView.toBack();
+    }
+
+    public static class LineInputData {
+        String name;
+        int weight;
+        public LineInputData(String name, int weight) {
+            this.name = name;
+            this.weight = weight;
+        }
+    }
+
+    public Optional<LineInputData> showConnectDialog(String fromName, String toName) {
+        Dialog<LineInputData> dialog = new Dialog<>();
+        dialog.setTitle("Create connection");
+        dialog.setHeaderText("Connect " + fromName + " with " + toName);
+
+        ButtonType okButtonType = new ButtonType("Connect", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(okButtonType, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField nameField = new TextField();
+        nameField.setPromptText("Subway line:");
+        TextField weightField = new TextField();
+        weightField.setPromptText("Travel time:");
+
+        grid.add(new Label("Subway line:"), 0, 0);
+        grid.add(nameField, 1, 0);
+        grid.add(new Label("Travel time (minutes):"), 0, 1);
+        grid.add(weightField, 1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == okButtonType) {
+                String name = nameField.getText().trim();
+                String weightString = weightField.getText().trim();
+
+                if (name.isEmpty()) {
+                    showErrorAlert("Invalid input", "Missing subway line name", "A valid name is required to make a connection");
+                    return null;
+                }
+
+                try {
+                    int weight = Integer.parseInt(weightString);
+                    if (weight <= 0) {
+                        showErrorAlert("Invalid input", "Invalid weight", "Travel time needs to be greater than 0");
+                        return null;
+                    }
+                    return new LineInputData(name, weight);
+                    
+                } catch (NumberFormatException e) {
+                    showErrorAlert("Invalid input", "Non-numerical weight", "Travel time needs to entered as an Integer");
+                    return null;
+                }
+            }
+            return null;
+        });
+
+        return dialog.showAndWait();
+    }
+
+    public void showErrorAlert(String title, String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 }
